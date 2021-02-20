a4f5f91a8ebc8edd3c48f9c68f8169c7af4038f8
kailafitz <mikhailafitzpatrick@yahoo.com>
2021-02-20 14:45:11 +0000

    Babel file deleted

--- src/Pages/Features.js
+++ src/Pages/Features.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import FeatureChecklistTable from '../Components/FeatureChecklistTable'
+import { FeaturesDetail } from '../Components/FeaturesDetail'
 import Footer from '../Components/Footer'
 import { ProductFeature } from '../Components/ProductFeature'
 
 export default function Features() {
     return (
         <> 
-            <ProductFeature />
-            <FeatureChecklistTable />
+            {/* <ProductFeature />
+            <FeatureChecklistTable /> */}
+            <FeaturesDetail />
             <Footer />
         </>
     )
