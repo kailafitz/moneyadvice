3a0242029fc821e997f6f88e59223859c13faae6
kailafitz <mikhailafitzpatrick@yahoo.com>
2021-03-06 17:00:00 +0000

    downloads page

--- src/Pages/Downloads.js
+++ src/Pages/Downloads.js
@@ -19,18 +19,20 @@ const DownloadsDiv = styled.div `
 `
 
 export const Downloads = () => {
-    <>
-        <SectionTitle title="Downloads" />
-        <DownloadsDiv>
-            <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_ProductBrochure" title="Product Brochure – Money Advice CRM" doc="MoneyAdviceCRM_ProductBrochure.pdf"/>
-            <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_AdditionalServices.pdf" title="Additional Broker Services" doc="MoneyAdviceCRM_AdditionalServices.pdf"/>
-            <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_MortgageProcess.pdf" title="Mortgage Process Guide" doc="MoneyAdviceCRM_MortgageProcess.pdf"/>
-            <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_DocumentScanner_UserGuide.pdf" title="Document Scanner User Guide" doc="MoneyAdviceCRM_DocumentScanner_UserGuide.pdf"/>
-            <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_NewCompliance_and_NeedsAnalysis_Process.pdf" title="New Compliance and Needs Analysis Process" doc="MoneyAdviceCRM_NewCompliance_and_NeedsAnalysis_Process.pdf"/>
-            <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_BankersOrders.pdf" title="Money Advice Bankers Order" doc="MoneyAdviceCRM_BankersOrders.pdf"/>
-            <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_Authorisation_Formw.pdf" title="Money Advice Authorisation Form" doc="MoneyAdviceCRM_Authorisation_Form.pdf"/>
-        </DownloadsDiv>
-        <ScrollTopButton />
-        <Footer />
-    </>
+    return (
+        <>
+            <SectionTitle title="Downloads" />
+            <DownloadsDiv>
+                <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_ProductBrochure" title="Product Brochure – Money Advice CRM" doc="MoneyAdviceCRM_ProductBrochure.pdf"/>
+                <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_AdditionalServices.pdf" title="Additional Broker Services" doc="MoneyAdviceCRM_AdditionalServices.pdf"/>
+                <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_MortgageProcess.pdf" title="Mortgage Process Guide" doc="MoneyAdviceCRM_MortgageProcess.pdf"/>
+                <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_DocumentScanner_UserGuide.pdf" title="Document Scanner User Guide" doc="MoneyAdviceCRM_DocumentScanner_UserGuide.pdf"/>
+                <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_NewCompliance_and_NeedsAnalysis_Process.pdf" title="New Compliance and Needs Analysis Process" doc="MoneyAdviceCRM_NewCompliance_and_NeedsAnalysis_Process.pdf"/>
+                <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_BankersOrders.pdf" title="Money Advice Bankers Order" doc="MoneyAdviceCRM_BankersOrders.pdf"/>
+                <DownloadMediaCard src="../Downloads/MoneyAdviceCRM_Authorisation_Formw.pdf" title="Money Advice Authorisation Form" doc="MoneyAdviceCRM_Authorisation_Form.pdf"/>
+            </DownloadsDiv>
+            <ScrollTopButton />
+            <Footer />
+        </>
+    );
 }
